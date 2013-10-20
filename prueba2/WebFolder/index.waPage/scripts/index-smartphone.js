3daed8650390eb498479dd1f35e372bc43583d9b
Your_Wakanda_Developer_Name_Here <WakandaDeveloper@wakanda.org>
2013-10-19 20:31:36 -0500

    Update #3

---
+++ prueba2/WebFolder/index.waPage/scripts/index-smartphone.js
@@ -0,0 +1,18 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var button2 = {};	// @button
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	button2.click = function button2_click (event)// @startlock
+	{// @endlock
+		$$('navigationView1').goToNextView();
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("button2", "click", button2.click, "WAF");
+// @endregion
+};// @endlock
